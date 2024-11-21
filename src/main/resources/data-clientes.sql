828f76436582ff856166f9048e369681b15a7340
juanvictorDev <juanvictor.dev@gmail.com>
2024-11-20 23:24:16 -0300

    melhoria: separacao data.sql em clientes e linhas + app.properties

---
+++ src/main/resources/data-clientes.sql
@@ -0,0 +1,48 @@
+
+-- CLIENTE Nº1
+    -- agencia: 2345-6
+    -- conta: 7654321-0
+    -- senha: senha123
+    
+INSERT INTO cliente_dados (
+    id_cliente, nome, cpf, email, senha, telefone, data_de_nascimento,
+    cep, estado, cidade, bairro, rua,numero_residencia, role, pcd, sexo, escolaridade,
+    estado_civil, ocupacao, renda_mensal
+) VALUES (
+    1, 'Pedro Paulo', '551.571.464-42', 'ppdev@gmail.com', '$2a$10$5j3dZ5D7MblH0GXF7aqv5ulZkAdjMAdSGvceppMWxi.q.A3UX7Twe',
+    '(81)99342-6942', '1997-05-10', '50800-010', 'PE', 'Recife',
+    'Iputinga', 'Rua Professor Joaquim Cavalcanti', '48', 'ROLE_USER', null, 'MASCULINO',
+    'ENSINO_SUPERIOR', 'SOLTEIRO', 'DESENVOLVEDOR_DE_SOFTWARE', 7546.45
+);
+
+INSERT INTO cliente_perfil (id_perfil, nota_do_perfil, score, avaliacao, id_cliente)
+VALUES (1, 7.4, 766, 'BOM', 1);
+
+INSERT INTO cliente_conta (id_conta, agencia, numero_da_conta, saldo, id_cliente)
+VALUES (1, "2345-6", "7654321-0", 10500.95, 1);
+
+
+
+
+
+-- CLIENTE Nº2
+    -- agencia: 1334-9
+    -- conta: 1234567-8
+    -- senha odontoPorAmorS2
+
+INSERT INTO cliente_dados (
+    id_cliente, nome, cpf, email, senha, telefone, data_de_nascimento,
+    cep, estado, cidade, bairro, rua, numero_residencia, role, pcd, sexo, escolaridade,
+    estado_civil, ocupacao, renda_mensal
+) VALUES (
+    2, 'Maria Silva', '123.456.789-00', 'maria.silva@gmail.com', '$2a$10$t05kLC0IdBV9LVQMD.6YiuYhThpZ7shjtJjhfm4D/FtWvqr/xFxW2',
+    '(81)91234-5678', '1990-08-15', '52390-210', 'PE', 'Recife',
+    'Passarinho', 'Rua Santa Fé', '100', 'ROLE_USER', null, 'FEMININO',
+    'POS_GRADUACAO', 'CASADO', 'DENTISTA', 6500.00
+);
+
+INSERT INTO cliente_perfil (id_perfil, nota_do_perfil, score, avaliacao, id_cliente)
+VALUES (2, 8.2, 805, 'EXCELENTE', 2);
+
+INSERT INTO cliente_conta (id_conta, agencia, numero_da_conta, saldo, id_cliente)
+VALUES (2, "1334-9", "1234567-8", 5000.00, 2);
--- src/main/resources/data-linhas.sql
+++ src/main/resources/data-linhas.sql
@@ -1,206 +1,160 @@
-INSERT INTO linha_de_credito (nome, descricao, imagem_nome, link_site, tipo)
+
+INSERT INTO linha_de_credito (nome, descricao, taxa_de_juros, imagem_nome, link_site, tipo)
 VALUES (
     '13º Salário',
     'Para quem recebe o salário pelo BB, a antecipação do 13º está disponível sem pagamento de parcelas mensais.',
+    1.9,
     '13-salario-image',
     'https://www.bb.com.br/site/pra-voce/emprestimo/emprestimo-pessoal/antecipar-13-salario/',
-    'ANTECIPACAO'
+    'ANTECIPAR_DECIMO_TERCEIRO'
 );
-INSERT INTO linha_de_credito (nome, descricao, imagem_nome, link_site, tipo)
+INSERT INTO linha_de_credito (nome, descricao, taxa_de_juros, imagem_nome, link_site, tipo)
 VALUES (
     'Restituição do IRPF', 
     'Antecipe o crédito de restituição do seu Imposto de Renda para dar uma forcinha nas contas.',
+    2.5,
     'restituicao-irpf-image',
     'https://www.bb.com.br/site/pra-voce/emprestimo/emprestimo-pessoal/antecipacao-irpf/',
-    'ANTECIPACAO'
+    'ANTECIPAR_IRPF'
 );
-INSERT INTO linha_de_credito (nome, descricao, imagem_nome, link_site, tipo)
+INSERT INTO linha_de_credito (nome, descricao, taxa_de_juros, imagem_nome, link_site, tipo)
 VALUES (
     'Saque Aniversário FGTS',
     'Se você aderiu à modalidade do Saque Aniversário, com o BB é possível antecipar o crédito sem pagar prestações.',
+    1.33,
     'saque-fgts-image',
     'https://www.bb.com.br/site/pra-voce/emprestimo/emprestimo-pessoal/antecipar-fgts/',
-    'ANTECIPACAO'
+    'ANTECIPAR_FGTS'
 );
 
 
 
 
 
-INSERT INTO linha_de_credito (nome, descricao, imagem_nome, link_site, tipo)
+INSERT INTO linha_de_credito (nome, descricao, taxa_de_juros, imagem_nome, link_site, tipo)
 VALUES (
     'Empréstimo com Garantia de Veículo', 
     'Empréstimo com taxas mais baixas, com o seu veículo como garantia.',
+    1.25,
     'emprestimo-garantia-veiculo-image',
     'https://www.bb.com.br/site/pra-voce/emprestimo/emprestimo-pessoal/emprestimo-com-garantia/emprestimo-com-garantia-de-veiculo/#/',
-    'GARANTIA'
+    'GARANTIA_VEICULO'
 );
-INSERT INTO linha_de_credito (nome, descricao, imagem_nome, link_site, tipo)
+INSERT INTO linha_de_credito (nome, descricao, taxa_de_juros, imagem_nome, link_site, tipo)
 VALUES (
     'Empréstimo com Garantia de Imóvel', 
     'Crédito para usar como quiser e o seu imóvel é a garantia da operação.',
+    1.05,
     'emprestimo-garantia-imovel-image',
     'https://www.bb.com.br/site/pra-voce/emprestimo/emprestimo-pessoal/emprestimo-com-garantia/emprestimo-com-garantia-de-imovel/#/',
-    'GARANTIA'
+    'GARANTIA_IMOVEL'
 );
-INSERT INTO linha_de_credito (nome, descricao, imagem_nome, link_site, tipo)
+INSERT INTO linha_de_credito (nome, descricao, taxa_de_juros, imagem_nome, link_site, tipo)
 VALUES (
     'Empréstimo Pessoal com Garantia de Investimentos e Previdência', 
     'Para quem tem aplicação financeira e deseja contratar um empréstimo. A aplicação é dada em garantia da operação.',
+    1.1,
     'emprestimo-garantia-investimentos-image',
     'https://www.bb.com.br/site/pra-voce/emprestimo/emprestimo-pessoal/emprestimo-com-garantia/emprestimo-com-garantia-de-investimentos/',
-    'GARANTIA'
+    'GARANTIA_INVESTIMENTO'
 );
 
 
 
 
-
-INSERT INTO linha_de_credito (nome, descricao, imagem_nome, link_site, tipo)
-VALUES (
-    'Empréstimo Consignado', 
-    'Crédito pessoal com desconto direto no seu contracheque ou no benefício do INSS.',
-    'emprestimo-consignado-image',
-    'https://www.bb.com.br/site/pra-voce/emprestimo/emprestimo-pessoal/emprestimo-consignado/#/',
-    'CDC'
-);
-INSERT INTO linha_de_credito (nome, descricao, imagem_nome, link_site, tipo)
+INSERT INTO linha_de_credito (nome, descricao, taxa_de_juros, imagem_nome, link_site, tipo)
 VALUES (
     'Empréstimo automático', 
     'Dinheiro na hora, taxas atrativas e até 59 dias para começar a pagar!',
+    2.89,
     'emprestimo-automatico-image',
     'https://www.bb.com.br/site/pra-voce/emprestimo/emprestimo-pessoal/emprestimo-automatico/#/',
-    'CDC'
+    'CREDITO_AUTOMATICO'
 );
-INSERT INTO linha_de_credito (nome, descricao, imagem_nome, link_site, tipo)
+INSERT INTO linha_de_credito (nome, descricao, taxa_de_juros, imagem_nome, link_site, tipo)
 VALUES (
     'Crédito Salário', 
     'Exclusivo para quem recebe o salário pelo BB, com pagamento da primeira parcela em até 180 dias e opção de cashback direto em conta!',
+    2.51,
     'credito-salario-image',
     'https://www.bb.com.br/site/pra-voce/emprestimo/emprestimo-pessoal/credito-salario/#/',
-    'CDC'
+    'CREDITO_SALARIO'
 );
-INSERT INTO linha_de_credito (nome, descricao, imagem_nome, link_site, tipo)
+INSERT INTO linha_de_credito (nome, descricao, taxa_de_juros, imagem_nome, link_site, tipo)
 VALUES (
     'Crédito Benefício', 
     'Aposentados e pensionistas do INSS garantem condições exclusivas de empréstimo.',
+    1.66,
     'credito-beneficio-image',
     'https://www.bb.com.br/site/pra-voce/emprestimo/emprestimo-pessoal/bb-credito-beneficio/',
-    'CDC'
+    'CREDITO_BENEFICIO'
 );
 
 
 
 
 
-INSERT INTO linha_de_credito (nome, descricao, imagem_nome, link_site, tipo)
+INSERT INTO linha_de_credito (nome, descricao, taxa_de_juros, imagem_nome, link_site, tipo)
 VALUES (
     'Financiamento Imobiliário', 
     'BB te ajuda a financiar seu imóvel com uma contratação simples e 100% digital.',
+    1.05,
     'financiamento-imovel-image',
     'https://www.bb.com.br/site/pra-voce/financiamentos/financiamento-imobiliario/',
-    'FINANCIAMENTO'
+    'FINANCIAMENTO_IMOBILIARIO'
 );
-INSERT INTO linha_de_credito (nome, descricao, imagem_nome, link_site, tipo)
+INSERT INTO linha_de_credito (nome, descricao, taxa_de_juros, imagem_nome, link_site, tipo)
 VALUES (
     'Financiamento de Veículos', 
     'Financie o carro dos seus sonhos com crédito do BB.',
+    1.02,
     'financiamento-veiculo-image',
     'https://www.bb.com.br/site/pra-voce/financiamentos/financiamento-de-carro/',
-    'FINANCIAMENTO'
+    'FINANCIAMENTO_CARRO'
 );
-INSERT INTO linha_de_credito (nome, descricao, imagem_nome, link_site, tipo)
+INSERT INTO linha_de_credito (nome, descricao, taxa_de_juros, imagem_nome, link_site, tipo)
 VALUES (
     'Financiamento de Motos', 
     'Taxas exclusivas pra você comprar ou trocar de moto.',
+    1.02,
     'financiamento-moto-image',
     'https://www.bb.com.br/site/pra-voce/financiamentos/financiamento-de-motos/',
-    'FINANCIAMENTO'
+    'FINANCIAMENTO_MOTO'
 );
 
-INSERT INTO linha_de_credito (nome, descricao, imagem_nome, link_site, tipo)
+INSERT INTO linha_de_credito (nome, descricao, taxa_de_juros, imagem_nome, link_site, tipo)
 VALUES (
     'Crédito Mobilidade', 
     'Muitas vantagens para adquirir sua bicicleta, patinete ou moto elétrica.',
+    1.83,
     'credito-mobilidade-image',
     'https://www.bb.com.br/site/pra-voce/financiamentos/bb-credito-mobilidade/',
-    'FINANCIAMENTO_BENS_SERVICOS'
+    'CREDITO_MOBILIDADE'
 );
-INSERT INTO linha_de_credito (nome, descricao, imagem_nome, link_site, tipo)
+INSERT INTO linha_de_credito (nome, descricao, taxa_de_juros, imagem_nome, link_site, tipo)
 VALUES (
     'Crédito Realiza', 
     'Pra você viajar, trocar seu eletrônico ou adquirir um novo.',
+    2.11,
     'credito-realiza-image',
     'https://www.bb.com.br/site/pra-voce/financiamentos/bb-credito-realiza/',
-    'FINANCIAMENTO_BENS_SERVICOS'
+    'CREDITO_REALIZA'
 );
-INSERT INTO linha_de_credito (nome, descricao, imagem_nome, link_site, tipo)
+INSERT INTO linha_de_credito (nome, descricao, taxa_de_juros, imagem_nome, link_site, tipo)
 VALUES (
     'Crédito Energia Renovável', 
     'O futuro é a sustentabilidade. Com o crédito energia renovável, você ajuda o meio ambiente e as gerações futuras.',
+    0.75,
     'credito-energia-renovavel-image',
     'https://www.bb.com.br/site/pra-voce/financiamentos/bb-credito-energia-renovavel/',
-    'FINANCIAMENTO_BENS_SERVICOS'
+    'CREDITO_ENERGIA_RENOVAVEL'
 );
-INSERT INTO linha_de_credito (nome, descricao, imagem_nome, link_site, tipo)
+INSERT INTO linha_de_credito (nome, descricao, taxa_de_juros, imagem_nome, link_site, tipo)
 VALUES (
     'Bens e Serviços para PCDs', 
     'Crédito para melhorar a qualidade de vida de pessoas com deficiência e suas famílias.',
+    0.43,
     'credito-pcd-image',
     'https://www.bb.com.br/site/pra-voce/financiamentos/produtos-e-servicos-para-pessoas-com-deficiencia/',
-    'FINANCIAMENTO_BENS_SERVICOS'
-);
-INSERT INTO linha_de_credito (nome, descricao, imagem_nome, link_site, tipo)
-VALUES (
-    'Pronaf Grupo A/C', 
-    'Custeio para assentados da Reforma Agrária (PNRA) ou para aqueles que adquiriram terras pelo Crédito Fundiário (PNCF). Além de apoiar as atividades indígenas e quilombolas.',
-    'pronaf-grupo-ac-image',
-    'https://www.bb.com.br/site/agronegocios/custeio/pronaf-custeio-a-c/',
-    'FINANCIAMENTO_AGRO'
-);
-INSERT INTO linha_de_credito (nome, descricao, imagem_nome, link_site, tipo)
-VALUES (
-    'Pronaf Agricultor Familiar', 
-    'Crédito para agricultores familiares enquadrados no Pronaf e com DAP ou CAF Pronaf ativos, voltado para o custeio de despesas com a produção agrícola ou pecuária',
-    'pronaf-agricultor-familiar-image',
-    'https://www.bb.com.br/site/agronegocios/custeio/pronaf-custeio/#/',
-    'FINANCIAMENTO_AGRO'
-);
-INSERT INTO linha_de_credito (nome, descricao, imagem_nome, link_site, tipo)
-VALUES (
-    'Crédito Rural Pronamp Custeio', 
-    'Financiamento de custeio para médios produtores (renda bruta anual de até R$3 milhões).',
-    'pronamp-custeio-image',
-    'https://www.bb.com.br/pbb/pagina-inicial/agronegocios/agronegocio---produtos-e-servicos/credito/credito-para-custeio/pronamp-custeio#/',
-    'FINANCIAMENTO_AGRO'
-);
-INSERT INTO linha_de_credito (nome, descricao, imagem_nome, link_site, tipo)
-VALUES (
-    'Custeio Agropecuário', 
-    'Crédito para as despesas de produção das atividades agrícolas e pecuárias.',
-    'custeio-agropecuario-image',
-    'https://www.bb.com.br/site/agronegocios/custeio-agropecuario/#/',
-    'FINANCIAMENTO_AGRO'
-);
-
-
-
-
-INSERT INTO linha_de_credito (nome, descricao, imagem_nome, link_site, tipo)
-VALUES (
-    'Renovação de Emprestimo', 
-    'Renove seus empréstimos, unifique seus contratos e ainda leve dinheiro extra para usar como quiser.',
-    'renovacao-emprestimo-image',
-    'https://www.bb.com.br/site/pra-voce/emprestimo/organizar-meus-compromissos/renovacao-de-emprestimos/',
-    'RENOVACAO'
-);
-
-
-INSERT INTO linha_de_credito (nome, descricao, imagem_nome, link_site, tipo)
-VALUES (
-    'Portabilidade de empréstimo', 
-    'Transferindo seus empréstimos para o BB, você pode obter melhores condições negociais!',
-    'portabilidade-emprestimo-image',
-    'https://www.bb.com.br/site/pra-voce/emprestimo/portabilidade-de-emprestimo/',
-    'PORTABILIDADE'
+    'BENS_SERVICOS_PCD'
 );
